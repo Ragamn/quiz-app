be0601d91086ec5284ec59e82776a664ea227a30
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2025-07-07 10:33:41 +0900

    migrationファイルを作成

---
+++ database/migrations/2025_07_07_011317_create_quizzes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id('quiz_id');
+            $table->foreignId('level_id')->constrained('levels', 'level_id')->onDelete('cascade');
+            $table->string('question');
+            $table->string('explanation');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
