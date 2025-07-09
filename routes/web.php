f9951e286dc2c20eaa18733565b526261d7cd395
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2025-07-09 12:10:58 +0900

    ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Levelコントローラーのルーティング
+Route::controller(LevelController::class)->group(function () {
+    Route::get('/top', 'selectAllLevels');
 });
