fe45dea36c70cbe60543a6cdae5257c01dc0c9cf
kuya <k.oda.sys22@morijyobi.ac.jp>
2025-07-09 12:01:50 +0900

    ログイン機能実装

--- database/migrations/2025_07_07_011402_create_scores_table.php
+++ database/migrations/2025_07_07_011402_create_scores_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->id('score_id');
-            $table->foreignId('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users', 'id')->onDelete('cascade');
             $table->foreignId('level_id')->constrained('levels', 'level_id')->onDelete('cascade');
             $table->integer('score');
             $table->timestamp('created_at');
