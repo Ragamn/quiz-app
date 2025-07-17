86fdd87765a6016f3163cfc1deea99f7e00be665
kuya <k.oda.sys22@morijyobi.ac.jp>
2025-07-17 14:12:04 +0900

    アカウント登録画面実装

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,75 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/background.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@900&display=swap" rel="stylesheet">
+    <title>ユーザー登録</title>
+</head>
+<body>
+    <h1 id="title">サスモン</h1>
+    <h2 id="display_title">しんきとうろく</h2>
+    
+    <form method="POST" action="{{ route('register') }}" class="reg_form">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <!-- <label for="name">名前</label><br> -->
+            <input id="name" type="text" name="name" class="input" placeholder="なまえ" value="{{ old('name') }}" required autofocus autocomplete="name" />
+            @if ($errors->get('name'))
+                <div style="color: red;">
+                    @foreach ($errors->get('name') as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div>
+            <!-- <label for="email">メールアドレス</label><br> -->
+            <input id="email" type="email" name="email" class="input" placeholder="メールアドレス" value="{{ old('email') }}" required autocomplete="username" />
+            @if ($errors->get('email'))
+                <div style="color: red;">
+                    @foreach ($errors->get('email') as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div>
+            <!-- <label for="password">パスワード</label><br> -->
+            <input id="password" type="password" name="password" class="input" placeholder="パスワード" required autocomplete="new-password" />
+            @if ($errors->get('password'))
+                <div style="color: red;">
+                    @foreach ($errors->get('password') as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        <div>
+            <!-- <label for="password_confirmation">パスワード確認</label><br> -->
+            <input id="password_confirmation" type="password" name="password_confirmation" class="input" placeholder="パスワードかくにん" required autocomplete="new-password" />
+            @if ($errors->get('password_confirmation'))
+                <div style="color: red;">
+                    @foreach ($errors->get('password_confirmation') as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div>
+            <button type="submit" id="login_btn">
+               <span>新規登録</span>
+            </button>
         </div>
     </form>
-</x-guest-layout>
+</body>
+</html>
