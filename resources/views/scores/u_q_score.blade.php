9fd39a4bf760495680745ffad8a60d04f8749931
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2025-07-17 14:09:47 +0900

    アコーディオンの元作成

---
+++ resources/views/scores/u_q_score.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/u_q_score.css') }}">
+    <title>スコア表示画面</title>
+</head>
+<body id="u_q_score">
+    <a href="">さいしょにもどる</a>
+    <h1>てんすう：80てん</h1>
+    <details name="accordion">
+        <summary>
+            <p class="summary-date">2025/07/16</p>
+            <div class="summary-header">
+                <h1 class="summary-title">クイズ1</h1>
+                <h1 class="summary-corect"> 〇あたり！</h1>
+            </div>
+        </summary>
+        SDGsは何個の目標がありますか？
+    </details>
+</body>
+</html>
